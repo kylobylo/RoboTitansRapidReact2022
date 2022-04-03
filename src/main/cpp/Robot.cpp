5064e98d0d8deb416ffdddacf5e0ac3c4407b051
kylobylo <49835859+kylobylo@users.noreply.github.com>
2022-04-02 20:02:01 -0500

    Functioning climb mode
    Climbing is actually functional and implemented.

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -6,6 +6,74 @@
 /*----------------------------------------------------------------------------*/
 
 #include "Robot.h"
+<<<<<<< Updated upstream
+=======
+#include <frc/Joystick.h>
+#include <rev/CANSparkMax.h>
+#include <frc/drive/DifferentialDrive.h>
+#include <iostream>
+#include <cameraserver/CameraServer.h>
+#include <frc/DigitalInput.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <frc/PneumaticsControlModule.h>
+#include <frc/Compressor.h>
+#include <frc/motorcontrol/Spark.h>
+#include <frc/Solenoid.h>
+#include <frc/DigitalInput.h>
+#include <fstream>
+#include <cscore_oo.h>
+#include "Debug.h"
+#include "Climb.h"
+#include "TankDrive.h"
+
+  //Put pin numbers as variables here.
+  unsigned const short driveStickID = 0;
+  unsigned const short controlStickID = 1;
+  unsigned const short controlTriggerID = 0;
+  unsigned const short sideButtonID = 2;
+  unsigned const short modeButtonID = 7;
+  //The Lead motor is the one in front of the lagging motor.
+  static unsigned const short leadRightSparkID = 1;
+  static unsigned const short leadLeftSparkID = 2;
+  static unsigned const short shooterMotorID = 3;
+  static unsigned const short intakeMotorID = 6;
+  //Define Spark and Spark Max objects
+  rev::CANSparkMax m_leftLeadingMotor{leadLeftSparkID, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax m_rightLeadingMotor{leadRightSparkID, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax m_shooterMotor{shooterMotorID, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax m_intakeMotor{intakeMotorID, rev::CANSparkMax::MotorType::kBrushless};
+  frc::Spark m_indexBelt{1};
+  //will follow the leading motors.
+  frc::DifferentialDrive m_robotDrive{m_leftLeadingMotor, m_rightLeadingMotor};
+  //Fine control differential drive object is needed for other joystick
+  tankDrive m_robotControl(&m_leftLeadingMotor, &m_rightLeadingMotor);
+  //Instantiate the left joystick
+  frc::Joystick m_climbStick{driveStickID};
+  //Instantiate the control joystick
+  frc::Joystick m_controlStick{controlStickID};
+  //Instantiate shooter speed variable for manipulation in Robot Periodic
+  float shooterSpeed = 1.0;
+  //create a Digital Input object to get grip detection
+  frc::DigitalInput testInput{6};
+
+  //create compressor
+  frc::Compressor pcmCompressor{0, frc::PneumaticsModuleType::CTREPCM};
+
+  //Create a mode toggle for manual climb control and a mode toggle for intake direction
+  bool buttonPressed = false;
+  bool testBool = false;
+  bool intake;
+  bool climbing;
+
+  climb climbObject;
+
+  //Previous accelerometer values
+  double prevXAccel = 0;
+  double prevYAccel = 0;
+
+  //frc::SendableChooser testChoices;
+
+>>>>>>> Stashed changes
 
 #include <iostream>
 
@@ -59,7 +127,62 @@ void Robot::AutonomousPeriodic() {
   }
 }
 
+<<<<<<< Updated upstream
 void Robot::TeleopInit() {}
+=======
+void Robot::TeleopInit() {
+  //Shooter speed is set so we don't start with a spinning motor.
+  shooterSpeed = 0.0;
+  frc::SmartDashboard::PutNumber("Shooter Speed", shooterSpeed);
+}
+
+void Robot::DisabledPeriodic() {
+
+}
+
+void Robot::TeleopPeriodic() {
+
+  //climber.prepareClimb(climbing);
+
+//NOT-YET-TESTED tankdrive class.  Easily replacible using frc::arcadedrive(might not be exaxt function call).
+  if (m_controlStick.GetY()>=0.1 || m_controlStick.GetY()<=-0.1 || m_controlStick.GetTwist()>=0.1 || m_controlStick.GetTwist()<=-0.1) {
+    m_robotDrive.ArcadeDrive(m_controlStick.GetTwist(),-1 * m_controlStick.GetY());
+  } 
+//Toggles intake to reject balls
+  if (m_controlStick.GetRawButtonReleased(8)) {
+    intake = !intake;
+  }
+  if (intake == true) {
+    m_intakeMotor.Set(1);
+  } else {
+    m_intakeMotor.Set(0);
+  }
+  if (m_climbStick.GetRawButtonPressed(8)) {
+    climbObject.prepareClimb(&climbing);
+  }
+
+  if (climbing == true) {
+    climbObject.doClimb(&m_climbStick);
+  }
+  
+
+  //Sets shooter speed with (hopefully) working limits and output to smart dashboard.
+  if (m_climbStick.GetRawButtonPressed(3) && shooterSpeed != 0.0 && climbing == false) {
+    shooterSpeed = shooterSpeed - 0.1; 
+    frc::SmartDashboard::PutNumber("Shooter Speed", shooterSpeed);
+  }
+  if (m_climbStick.GetRawButtonPressed(4) && shooterSpeed != 1.0 && climbing == false) {
+    shooterSpeed = shooterSpeed + 0.1;
+    frc::SmartDashboard::PutNumber("Shooter Speed", shooterSpeed);
+  }
+  m_shooterMotor.Set(shooterSpeed);
+  if (m_controlStick.GetRawButton(4)) {
+    m_indexBelt.Set(1.0);
+  } else {
+    m_indexBelt.Set(0.0);
+  }
+}
+>>>>>>> Stashed changes
 
 void Robot::TeleopPeriodic() {}
 
